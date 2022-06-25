2d1dee081fe476eca5d5faee7870396388cb9184
Louis-Frenzel <104778980+Louis-Frenzel@users.noreply.github.com>
2022-06-25 11:29:32 +0200

    Add files via upload

---
+++ Meilenstein/JS/JSKontakt.js
@@ -0,0 +1,11 @@
+function kontaktformular(){
+    //console.log("hats geklappt?")
+    var mail;
+    var name;
+    var text;
+    mail = document.getElementById("kontaktmail").value;
+    name = document.getElementById("kontaktname").value;
+    text = document.getElementById("eingabebox").value;
+    console.log(mail + " " + name + " " + text);
+    
+}
